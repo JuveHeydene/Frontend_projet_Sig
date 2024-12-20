26563983dc996a74337766cb59460b6fdb269af1
NyemeckJames <jamesnyemeck@gmail.com>
2024-12-20 22:11:22 +0100

    Gestion de l'affichage des items de la sidebar en fonction du role de l'utilisateur

--- frontend_sig/app/components/sidebar/Sidebar.tsx
+++ frontend_sig/app/components/sidebar/Sidebar.tsx
@@ -4,6 +4,13 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import "./Sidebar.scss";
+interface MenuItem {
+  id: string;
+  icon: string;
+  label: string|null;
+  children: Array<MenuItem>|null;
+  url: string;
+}
 
 const Sidebar = (role:any) => {
   const sidebarRef = useRef<HTMLElement>(null);
@@ -35,37 +42,224 @@ const Sidebar = (role:any) => {
       toggleBtnRef.current?.classList.toggle('rotate')
     }
   }
-  const menuItems = [
-    { id: "Menu", icon: "keyboard_double_arrow_left", label: null, children: null },
-    { id: "Dashboard", icon: "dashboard", label: "Dashboard", children: null },
-    { id: "HomePage", icon: "home", label: "Home Page", children: null },
-    { id: "AllUsers", icon: "people", label: "All Users", children: null },
-    { id: "Create", icon: "add", label: "Create", children:[
-      {
-        id: "CreateUsers",
-        icon: "person_add",
-        label: "Create Users",
-        children: null,
-      },
-      {
-        id: "VotingCenter",
-        icon: "apartment",
-        label: "Create Center",
-        children: null,
-      },
-      {
-        id: "Favorite",
-        icon: "how_to_vote",
-        label: "Create VoteOffice",
-        children: null,
-      },
-    ] },
-    { id: "Settings", icon: "settings", label: "Settings", children: null },
-    { id: "AdvancedParameters", icon: "tune", label: "ADVANCE PARAMETERS", children: null },
-    { id: "AboutElection", icon: "info", label: "About Election", children: null },
-    { id: "LightDarkMode", icon: "light_mode", label: "Light/Dark Mode", children: null },
-    { id: "Logout", icon: "logout", label: "Log Out", children: null },
-  ];
+  let menuItems:Array<MenuItem> = []
+  switch (role) {
+    case "ADMINISTRATEUR":
+      menuItems = [
+        { id: "Menu", icon: "keyboard_double_arrow_left", label: null, children: null ,url: "" },
+        { id: "Dashboard", icon: "dashboard", label: "Dashboard", children: null ,url: "" },
+        { id: "users", icon: "group", label: "Users", children:[
+          {
+            id: "CreateUsers",
+            icon: "person_add",
+            label: "Create Users",
+            children: null,
+            url: ""
+          },
+          {
+            id: "user-list",
+            icon: "people",
+            label: "Registered people",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "voting-centers", icon: "apartment", label: "Voting centers", children:[
+          {
+            id: "center-list",
+            icon: "list",
+            label: "Voting centers list",
+            children: null,
+            url: ""
+          },
+          {
+            id: "add-voting-centers",
+            icon: "add",
+            label: "New voting center",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "Voting-office", icon: "how_to_vote", label: "Voting office", children:[
+          {
+            id: "voting-office-list",
+            icon: "list",
+            label: "Voting office list",
+            children: null,
+            url: ""
+          },
+          {
+            id: "add-voting-office",
+            icon: "add",
+            label: "Voting office",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "voting-results", icon: "summarize", label: "Voting result", children:[
+          {
+            id: "consult-voting-result",
+            icon: "visibility",
+            label: "Consult results",
+            children: null,
+            url: ""
+          },
+          {
+            id: "insert-result",
+            icon: "edit",
+            label: "Insert a vote result",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "Settings", icon: "settings", label: "Settings", children: null  ,url: ""},
+        { id: "Logout", icon: "logout", label: "Log Out", children: null ,url: "" },
+      ];
+    case "ENROLLEUR":
+      menuItems = [
+        { id: "Menu", icon: "keyboard_double_arrow_left", label: null, children: null ,url: "" },
+        { id: "Dashboard", icon: "dashboard", label: "Dashboard", children: null ,url: "" },
+        { id: "users", icon: "group", label: "Enrolled electors", children:[
+          {
+            id: "CreateUsers",
+            icon: "person_add",
+            label: "Add electors",
+            children: null,
+            url: ""
+          },
+          {
+            id: "user-list",
+            icon: "people",
+            label: "Registered electors",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "Settings", icon: "settings", label: "Settings", children: null  ,url: ""},
+        { id: "Logout", icon: "logout", label: "Log Out", children: null ,url: "" },
+      ];
+    case "SCRUTATEUR":
+      menuItems = [
+        { id: "Menu", icon: "keyboard_double_arrow_left", label: null, children: null ,url: "" },
+        { id: "Dashboard", icon: "dashboard", label: "Dashboard", children: null ,url: "" },
+        { id: "voting-results", icon: "summarize", label: "Voting result", children:[
+          {
+            id: "consult-voting-result",
+            icon: "visibility",
+            label: "Consult office results",
+            children: null,
+            url: ""
+          },
+          {
+            id: "insert-result",
+            icon: "edit",
+            label: "Edit office result",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "Settings", icon: "settings", label: "Settings", children: null  ,url: ""},
+        { id: "Logout", icon: "logout", label: "Log Out", children: null ,url: "" },
+      ];
+    case "SUPERVISEUR":
+      menuItems = [
+        { id: "Menu", icon: "keyboard_double_arrow_left", label: null, children: null ,url: "" },
+        { id: "Dashboard", icon: "dashboard", label: "Dashboard", children: null ,url: "" },
+        { id: "users", icon: "group", label: "Users", children:[
+          {
+            id: "CreateUsers",
+            icon: "person_add",
+            label: "Create Users",
+            children: null,
+            url: ""
+          },
+          {
+            id: "user-list",
+            icon: "people",
+            label: "Registered people",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "Settings", icon: "settings", label: "Settings", children: null  ,url: ""},
+        { id: "Logout", icon: "logout", label: "Log Out", children: null ,url: "" },
+      ];
+    
+      break;
+  
+    default:
+      menuItems = [
+        { id: "Menu", icon: "keyboard_double_arrow_left", label: null, children: null ,url: "" },
+        { id: "Dashboard", icon: "dashboard", label: "Dashboard", children: null ,url: "" },
+        { id: "users", icon: "group", label: "Users", children:[
+          {
+            id: "CreateUsers",
+            icon: "person_add",
+            label: "Create Users",
+            children: null,
+            url: ""
+          },
+          {
+            id: "user-list",
+            icon: "people",
+            label: "Registered people",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "voting-centers", icon: "apartment", label: "Voting centers", children:[
+          {
+            id: "center-list",
+            icon: "list",
+            label: "Voting centers list",
+            children: null,
+            url: ""
+          },
+          {
+            id: "add-voting-centers",
+            icon: "add",
+            label: "New voting center",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "Voting-office", icon: "how_to_vote", label: "Voting office", children:[
+          {
+            id: "voting-office-list",
+            icon: "list",
+            label: "Voting office list",
+            children: null,
+            url: ""
+          },
+          {
+            id: "add-voting-office",
+            icon: "add",
+            label: "Voting office",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "voting-results", icon: "summarize", label: "Voting result", children:[
+          {
+            id: "consult-voting-result",
+            icon: "visibility",
+            label: "Consult results",
+            children: null,
+            url: ""
+          },
+          {
+            id: "insert-result",
+            icon: "edit",
+            label: "Insert a vote result",
+            children: null,
+            url: ""
+          }
+        ] , url: ""},
+        { id: "Settings", icon: "settings", label: "Settings", children: null  ,url: ""},
+        { id: "Logout", icon: "logout", label: "Log Out", children: null ,url: "" },
+      ];
+  }
+  
 
   
 
